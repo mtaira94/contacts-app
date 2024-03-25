c7f3e714137acc78e832c21f920f81f577f86381
Moises T <47845847+mtaira94@users.noreply.github.com>
2024-03-25 12:51:45 -0700

    Finished course

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
-import { Subscription } from 'rxjs';
+import { Subscription, debounce, debounceTime, distinctUntilChanged } from 'rxjs';
 import { Contact, addressTypeValues, phoneTypeValues } from '../contacts/contact.model';
 import { restrictedWords } from '../validators/restricted-words-validator';
 import { ProfileIconSelectorComponent } from '../profile-icon-selector/profile-icon-selector.component';
@@ -45,7 +45,10 @@ export class EditContactComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) {
+      this.subscribeToAddressChanges();
+      return
+    }
 
     this.contactSub = this.contacstService.getContact(contactId).subscribe(contact => {
       if (!contact) return
@@ -55,6 +58,8 @@ export class EditContactComponent implements OnInit, OnDestroy {
       }
 
       this.contactForm.setValue(contact);
+
+      this.subscribeToAddressChanges();
     });
   }
 
@@ -63,15 +68,36 @@ export class EditContactComponent implements OnInit, OnDestroy {
     // this.contactSaveSub.unsubscribe();
   }
 
-  addPhone(){
+  addPhone() {
     this.contactForm.controls.phones.push(this.createPhoneGroupt());
   }
 
+  stringifyCompare(a: any, b: any): boolean {
+    return JSON.stringify(a) === JSON.stringify(b);
+  }
+
   createPhoneGroupt() {
-    return this.fb.nonNullable.group({
+    const phoneGroup = this.fb.nonNullable.group({
       phoneNumber: '',
-      phoneType: ''
+      phoneType: '',
+      preferred: false
     });
+
+    phoneGroup.controls.preferred.valueChanges
+      .pipe(
+        distinctUntilChanged(this.stringifyCompare)
+      )
+      .subscribe(value => {
+        if (value) {
+          phoneGroup.controls.phoneNumber.addValidators([Validators.required])
+        } else {
+          phoneGroup.controls.phoneNumber.removeValidators([Validators.required])
+        }
+
+        phoneGroup.controls.phoneNumber.updateValueAndValidity();
+      });
+
+    return phoneGroup;
   }
 
   get firstName() {
@@ -80,6 +106,33 @@ export class EditContactComponent implements OnInit, OnDestroy {
 
   get notes() {
     return this.contactForm.controls.notes;
+  }
+
+  subscribeToAddressChanges() {
+    const addressGroup = this.contactForm.controls.address;
+
+    addressGroup.valueChanges
+      .pipe(
+        distinctUntilChanged(this.stringifyCompare)
+      )
+      .subscribe(() => {
+        for (const controlName in addressGroup.controls) {
+          addressGroup.get(controlName)?.removeValidators([Validators.required]);
+          addressGroup.get(controlName)?.updateValueAndValidity();
+        }
+      });
+
+    addressGroup.valueChanges
+      .pipe(
+        debounceTime(2000),
+        distinctUntilChanged(this.stringifyCompare)
+      )
+      .subscribe(() => {
+        for (const controlName in addressGroup.controls) {
+          addressGroup.get(controlName)?.addValidators([Validators.required]);
+          addressGroup.get(controlName)?.updateValueAndValidity();
+        }
+      });
   }
 
   saveContact() {
